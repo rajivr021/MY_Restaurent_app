d87108dc280e192657c8b97ca64bc923c8ac8d76
Rajiv <rajivr021@gmail.com>
2023-04-29 23:05:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Restaurant from './Component/Basics/Restaurant'
-
+import Header from '../src/Component/MYHeader/Header'
 const App = () => {
   return (
    <>
+   <Header/>
   <Restaurant/>
    </>
   )
---
+++ src/Component/MYHeader/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="MyHeader">
+     <div className='logo'>hello log</div>
+     <ul className="MidHeader">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About us</li>
+        <li>Order Online</li>
+        <li>Reservation</li>
+        <li>Contact us</li>
+     </ul>
+      
+    </div>
+  )
+}
+
+export default Header
